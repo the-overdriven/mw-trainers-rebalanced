8ac5470fefb99c9c6fb8416004aa784fda009616
the-overdriven <100090726+the-overdriven@users.noreply.github.com>
2024-12-24 00:46:18 +0100

    clean up

--- main.lua
+++ main.lua
@@ -2,14 +2,13 @@
 
 local isDebugOn = true
 
--- TODO prefix with G
-local trainerCurrent
-local trainerCurrentMobile
-local trainerCurrentId
-local trainingIterations
-local skillProgressRequirement = {}
+local g_trainerCurrent
+local g_trainerCurrentMobile
+local g_trainerCurrentId
+local g_trainingIterations
+local g_pcSkillProgressRequirement = {}
 
-local tierToExperience = {
+local tierLabels = {
   [1] = 'modest',
   [2] = 'interesting',
   [3] = 'meaningful',
@@ -23,8 +22,8 @@ local function log(...)
   end
 
   local filteredArgs = {}
-  for i = 1, select("#", ...) do -- select("#", ...) gets the total number of arguments
-    local v = select(i, ...)  -- Access each argument by its position
+  for i = 1, select('#', ...) do
+    local v = select(i, ...)
 
     if v == nil then
       v = 'NIL!'
@@ -36,8 +35,8 @@ local function log(...)
   mwse.log(table.unpack(filteredArgs))
 end
 
--- { unarmored: 1 } -> { 1: 'unarmored' }
--- skills[1] = alternative for tes3.getSkillName(1)
+--- skills[1] = alternative for tes3.getSkillName(1)
+--- { unarmored: 1 } -> { 1: 'unarmored' }
 local function invertTable(tbl)
   local inverted = {}
   for key, value in pairs(tbl) do
@@ -46,6 +45,30 @@ local function invertTable(tbl)
   return inverted
 end
 local skills = invertTable(tes3.skill)
+
+--- Rounds to nearest integer
+--- with .5 rounded down
+function roundHalfDown(value)
+  if value % 1 == 0.5 then
+    return math.floor(value)
+  else
+    return math.floor(value + 0.5)
+  end
+end
+
+--- Expects trainerSkillValue 1-100
+--- Returns trainerTier 1-5
+local function getTrainerTier(trainerSkillValue)
+  local trainerTier
+  if trainerSkillValue < 20 then
+    -- skill value 1-30
+    trainerTier = 1
+  else
+    -- skill 31-100 = tier 2-5 (master trainer = 5)
+    trainerTier = roundHalfDown(trainerSkillValue / 20)
+  end
+  return trainerTier
+end
 
 -- Restores trainer's original skill value.
 -- When training window is opened
@@ -56,18 +79,18 @@ local function restoreTrainerSkills()
   if not tes3.player.data.trainedAt then
     return
   end
-  if not tes3.player.data.trainedAt[trainerCurrentId] then
+  if not tes3.player.data.trainedAt[g_trainerCurrentId] then
     return
   end
 
   log('[TRU] Restoring trainer\'s skills.')
-  for skillId, skillValue in pairs(tes3.player.data.trainedAt[trainerCurrentId]) do
+  for skillId, skillValue in pairs(tes3.player.data.trainedAt[g_trainerCurrentId]) do
     log('[TRU] skillId: %s (%s), skillValue: %s', skillId, skills[skillId], skillValue)
-    tes3.setStatistic({ reference = tes3.getReference(trainerCurrentId), skill = skillId, value = skillValue })
+    tes3.setStatistic({ reference = tes3.getReference(g_trainerCurrentId), skill = skillId, value = skillValue })
   end
 end
 
--- patch for Right Click Menu Exit, should do the same as uiEventCallback, but right-click doesn't catch it
+--- Patch for Right Click Menu Exit: without it closing windows with right-click doesn't trigger corresponding events
 local function onMouseButtonDown(e)
   if tes3ui.menuMode() then
     if e.button == tes3.worldController.inputController.inputMaps[19].code then
@@ -78,8 +101,10 @@ local function onMouseButtonDown(e)
     end
   end
 end
-event.register("mouseButtonDown", onMouseButtonDown)
+event.register('mouseButtonDown', onMouseButtonDown)
 
+--- Triggers hiding selected skills when training window is opened
+--- and restoring selected skills when dialogue/training window is closed
 --- @param e uiEventEventData
 local function uiEventCallback(e)
   mwse.log('parent %s, property %s, source %s', e.parent, e.property, e.source)
@@ -91,6 +116,7 @@ local function uiEventCallback(e)
   }
 
   if (e.property == mouseDown) and closeButtons[tostring(e.parent)] then
+    -- restore skills when closing training window or dialogue window
     restoreTrainerSkills()
     return
   end
@@ -104,26 +130,23 @@ local function uiEventCallback(e)
 end
 event.register(tes3.event.uiEvent, uiEventCallback)
 
--- TODO: add requirement for ui expansion
--- TODO: test on master trainers
-
 -- local function onDialogueStart(e)
---   npcRef = e.element:getPropertyObject("PartHyperText_actor").reference
+--   npcRef = e.element:getPropertyObject('PartHyperText_actor').reference
 --   if npcRef.object.objectType ~= tes3.objectType.npc then
 --     npcRef = nil
 --     return
 --   end
 --   timer.delayOneFrame(function()
 --     log('[TRU] dialogue window OR training window opened')
---     log('[TRU][hideTrainerSkills] Unarmored skill (test) BEFORE: %s', npcRef.mobile:getSkillValue(tes3.skill['unarmored']))
 --     tes3.messageBox('NPC unarmored skill value %s', npcRef.mobile:getSkillValue(tes3.skill['unarmored']))
---     -- should NOT be 1 after training
+--     -- should NOT be 1 after training it
 --     -- hideTrainerSkills(npcRef)
 --   end)
 -- end
--- event.register("uiActivated", onDialogueStart, {filter = "MenuDialog"})
--- this is actually not needed anymore, only for tests
+-- event.register('uiActivated', onDialogueStart, {filter = 'MenuDialog'})
+-- this is actually not needed anymore, except tests
 
+--- Hides skills that a selected NPC has teached already
 function hideTrainerSkills(npcRef)
   log('[TRU][hideTrainerSkills] -----------------------------------')
 
@@ -132,61 +155,43 @@ function hideTrainerSkills(npcRef)
     log('[TRU][hideTrainerSkills] Mod used for the first time. CREATED tes3.player.data.trainedAt')
   end
 
-  trainerCurrent = npcRef
-  trainerCurrentId = trainerCurrent.id
-  trainerCurrentMobile = npcRef.mobile
-  mwse.log('[TRU][hideTrainerSkills] trainerCurrent: %s, trainerCurrentMobile: %s, trainerCurrentId: %s', trainerCurrent, trainerCurrentMobile, trainerCurrentId)
+  g_trainerCurrent = npcRef
+  g_trainerCurrentId = g_trainerCurrent.id
+  g_trainerCurrentMobile = npcRef.mobile
+  mwse.log('[TRU][hideTrainerSkills] g_trainerCurrent: %s, g_trainerCurrentId: %s, g_trainerCurrentMobile: %s', g_trainerCurrent, g_trainerCurrentId, g_trainerCurrentMobile)
 
   if (tes3.player.data.trainedAt 
-  and tes3.player.data.trainedAt[trainerCurrentId]) then
-    -- for each already trained skill in tes3.player.data.trainedAt[trainerCurrentId]
+  and tes3.player.data.trainedAt[g_trainerCurrentId]) then
+    -- for each already trained skill in tes3.player.data.trainedAt[g_trainerCurrentId]
     -- decrease current trainer's skill value to 1, to prevent skilling more than once
     -- TODO: this could be copied to skillRaisedCallback to refresh available skills sooner, but it might be confusing for player
-    for skillId, skillValue in pairs(tes3.player.data.trainedAt[trainerCurrentId]) do
-      log('[TRU][hideTrainerSkills] ALREADY trained skillId %s (%s) at %s, block it', skillId, skills[skillId], trainerCurrentId)
+    for skillId, skillValue in pairs(tes3.player.data.trainedAt[g_trainerCurrentId]) do
+      log('[TRU][hideTrainerSkills] ALREADY trained skillId %s (%s) at %s, block it', skillId, skills[skillId], g_trainerCurrentId)
       log('[TRU][hideTrainerSkills] trainer\'s skill BEFORE: %s', trainerSkillValueOriginal)
-      tes3.setStatistic({ reference = tes3.getReference(trainerCurrentId), skill = skillId, value = 1 })
-      log('[TRU][hideTrainerSkills] trainer\'s skill AFTER: %s', trainerCurrentMobile:getSkillValue(skillId))
+      tes3.setStatistic({ reference = tes3.getReference(g_trainerCurrentId), skill = skillId, value = 1 })
+      log('[TRU][hideTrainerSkills] trainer\'s skill AFTER: %s', g_trainerCurrentMobile:getSkillValue(skillId))
     end
   end
 end
 
-function bankersRound(value)
-  if value % 1 == 0.5 then
-    return math.floor(value)
-  else
-    return math.floor(value + 0.5)
-  end
-end
-
-local function getTrainerTier(trainerSkillValue)
-  local trainerTier
-  if trainerSkillValue < 20 then
-    -- skill value 1-30
-    trainerTier = 1
-  else
-    -- skill 31-100 = tier 2-5 (master trainer = 5)
-    trainerTier = bankersRound(trainerSkillValue / 20)
-  end
-  return trainerTier
-end
-
+--- Executed when training window appears
 --- @param e calcTrainingPriceEventData
 local function calcTrainingPriceCallback(e)
   log('[TRU][calcTrainingPriceCallback] -----------------------------------')
 
-  trainerCurrent = e.reference
-  trainerCurrentMobile = e.mobile
   local skillId = e.skillId
-  local trainerSkillValueOriginal = trainerCurrentMobile:getSkillValue(skillId)
+  local trainerSkillValueOriginal = g_trainerCurrentMobile:getSkillValue(skillId)
   local trainerTier = getTrainerTier(trainerSkillValueOriginal)
-  trainingIterations = nil
   e.price = e.price * trainerTier
-  skillProgressRequirement[skillId] = tes3.mobilePlayer:getSkillProgressRequirement(skillId)
 
-  log('[TRU][calcTrainingPriceCallback] trainerCurrent: %s', trainerCurrent)
+  g_trainingIterations = nil
+  g_trainerCurrent = e.reference
+  g_trainerCurrentMobile = e.mobile  
+  g_pcSkillProgressRequirement[skillId] = tes3.mobilePlayer:getSkillProgressRequirement(skillId)
+
+  log('[TRU][calcTrainingPriceCallback] g_trainerCurrent: %s', g_trainerCurrent)
   log('[TRU][calcTrainingPriceCallback] e.ref: %s, e.basePrice: %s, e.price: %s, e.skillId: %s (%s)', e.reference, e.basePrice, e.price, skillId, skills[skillId])
-  log('[TRU][calcTrainingPriceCallback] skillProgressRequirement[skillId]: %s (%s)', skillProgressRequirement[skillId], skills[skillId])
+  log('[TRU][calcTrainingPriceCallback] g_pcSkillProgressRequirement[skillId]: %s (%s)', g_pcSkillProgressRequirement[skillId], skills[skillId])
 end
 event.register(tes3.event.calcTrainingPrice, calcTrainingPriceCallback)
 
@@ -195,78 +200,77 @@ local function skillRaisedCallback(e)
   log('[TRU][skillRaisedCallback] -----------------------------------')
   log('[TRU][skillRaisedCallback] e.source: %s', e.source)
 
-  if e.source ~= 'training' and not (e.source == 'progress' and trainingIterations ~= nil) then
-    -- bumping skill programatically with mobile:progressSkillToNextLevel is treated as "progress"
+  if e.source ~= 'training' and not (e.source == 'progress' and g_trainingIterations ~= nil) then
+    -- bumping skill programatically with mobile:progressSkillToNextLevel is treated as 'progress'
     return
   end
 
   local skillId = e.skill
-  local trainerSkillValueOriginal = trainerCurrentMobile:getSkillValue(skillId)
+  local trainerSkillValueOriginal = g_trainerCurrentMobile:getSkillValue(skillId)
   local trainerTier = getTrainerTier(trainerSkillValueOriginal)
 
-  log('[TRU][skillRaisedCallback] trainingIterations left: %s (of %s)', trainingIterations, trainerTier)
-  log('[TRU][skillRaisedCallback] trained at NPC: %s', trainerCurrentId)
+  log('[TRU][skillRaisedCallback] g_trainingIterations left: %s (of %s)', g_trainingIterations, trainerTier)
+  log('[TRU][skillRaisedCallback] trained at NPC: %s', g_trainerCurrentId)
   log('[TRU][skillRaisedCallback] trained skill id: %s (%s)', skillId, skills[skillId])
   log('[TRU][skillRaisedCallback] trainerTier: %s', trainerTier)
   log('[TRU][skillRaisedCallback] trainer\'s skill value: %s', trainerSkillValueOriginal)
-  log('[TRU][skillRaisedCallback] trained to level: %s', e.level)  
+  log('[TRU][skillRaisedCallback] trained to level: %s', e.level)
 
-  if trainingIterations == nil then
+  if g_trainingIterations == nil then
     -- this is executed once per skill train loop, in FIRST iteration
     -- training is repeated, once per every trainer tier (max. 5 times)
-    trainingIterations = trainerTier
+    g_trainingIterations = trainerTier
 
     -- remember that this skill was trained at this NPC
     -- save value as trainer's original skill level, to reset it after closing training window
-    if (not tes3.player.data.trainedAt[trainerCurrentId]) then
-      tes3.player.data.trainedAt[trainerCurrentId] = {}
-      log('[TRU][skillRaisedCallback] trained at %s for the first time, CREATED tes3.player.data.trainedAt[trainerCurrentId]', trainerCurrent)
+    if (not tes3.player.data.trainedAt[g_trainerCurrentId]) then
+      tes3.player.data.trainedAt[g_trainerCurrentId] = {}
+      log('[TRU][skillRaisedCallback] trained at %s for the first time, CREATED tes3.player.data.trainedAt[g_trainerCurrentId]', g_trainerCurrent)
     end
-    log('[TRU][skillRaisedCallback] %s teached %s for the first time, CREATED tes3.player.data.trainedAt[trainerCurrentId][skillId]', trainerCurrent, skills[skillId])
-    tes3.player.data.trainedAt[trainerCurrentId][skillId] = trainerCurrentMobile:getSkillValue(skillId)
+    log('[TRU][skillRaisedCallback] %s teached %s for the first time, CREATED tes3.player.data.trainedAt[g_trainerCurrentId][skillId]', g_trainerCurrent, skills[skillId])
+    tes3.player.data.trainedAt[g_trainerCurrentId][skillId] = g_trainerCurrentMobile:getSkillValue(skillId)
 
-    -- move to last iteration?
     tes3.messageBox({
       message = string.format(
         'The training has payed off. %s has shared their %s experience (%s) about %s with you, and you\'ve improved from %s to %s. There is nothing more %s can teach you about %s. Take a break, ask about something else, or find other teacher.', 
-        trainerCurrentMobile.object.name,
-        tierToExperience[trainerTier],
+        g_trainerCurrentMobile.object.name,
+        tierLabels[trainerTier],
         trainerSkillValueOriginal,
         skills[skillId],
-        e.level-1, -- PC skill before training
-        e.level-1+trainingIterations, -- PC skill after training
-        trainerCurrentMobile.object.name,
+        e.level - 1, -- PC skill before training
+        e.level - 1 + g_trainingIterations, -- PC skill after training
+        g_trainerCurrentMobile.object.name,
         skills[skillId]
         ),
       buttons = { 'OK' }
     })
   end
 
-  if trainingIterations ~= nil and trainingIterations == 1 then
+  if g_trainingIterations ~= nil and g_trainingIterations == 1 then
     -- this is executed once per skill train loop, in LAST iteration
-    log('[TRU][skillRaisedCallback] trainingIterations finished for %s, reset trainingIterations', skills[skillId])
-    trainingIterations = nil
-    hideTrainerSkills(trainerCurrent) -- hide trained skill before training window reopens
+    log('[TRU][skillRaisedCallback] g_trainingIterations finished for %s, reset g_trainingIterations', skills[skillId])
+    g_trainingIterations = nil
+    hideTrainerSkills(g_trainerCurrent) -- hide trained skill before training window reopens
 
     -- take xp overflow into account!
     -- check if the skill that we're leveling up had any progress, and then add it
     -- current getSkillProgressRequirement is 100% (when on UI it's 0/100), 
-    -- so whatever progress was required before training (the old value in skillProgressRequirement[skillId])
+    -- so whatever progress was required before training (the old value in g_pcSkillProgressRequirement[skillId])
     -- needs to be subtracted from getSkillProgressRequirement and the result should be added to current progress 
     -- (for simplicity it assumes progress is linear, but in fact higher levels should have bigger requirements)
-    local xpOverflow = tes3.mobilePlayer:getSkillProgressRequirement(skillId) - skillProgressRequirement[skillId]
-    log('[TRU][skillRaisedCallback] tes3.mobilePlayer:getSkillProgressRequirement(skillId): %s, skillProgressRequirement[skillId]: %s', xpOverflow, skillProgressRequirement[skillId])
+    local xpOverflow = tes3.mobilePlayer:getSkillProgressRequirement(skillId) - g_pcSkillProgressRequirement[skillId]
+    log('[TRU][skillRaisedCallback] tes3.mobilePlayer:getSkillProgressRequirement(skillId): %s, g_pcSkillProgressRequirement[skillId]: %s', xpOverflow, g_pcSkillProgressRequirement[skillId])
     log('[TRU][skillRaisedCallback] xpOverflow: %s', xpOverflow)
     if not xpOverflow or xpOverflow == 0 then return end
-    skillProgressRequirement[skillId] = nil
+    g_pcSkillProgressRequirement[skillId] = nil
     tes3.mobilePlayer:exerciseSkill(skillId, xpOverflow)
 
     return
   end
 
-  trainingIterations = trainingIterations - 1
-  if trainingIterations > 0 then
-    -- repeat skillRaisedCallback for every trainingIterations left
+  g_trainingIterations = g_trainingIterations - 1
+  if g_trainingIterations > 0 then
+    -- repeat skillRaisedCallback for every g_trainingIterations left
     tes3.getPlayerRef().mobile:progressSkillToNextLevel(skillId)
   end
 end
